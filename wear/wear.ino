47b5cd1c910cef4cc6b8b618c1c82222ddebcfe6
Yourein <rein.you.3.1@gmail.com>
2024-12-15 04:54:59 +0900

    とりあえずシリアル受取を実装してみた

--- wear/wear.ino
+++ wear/wear.ino
@@ -9,6 +9,8 @@
 WearableState currentState = WearableState::Waiting;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(49, 6, NEO_GRB + NEO_KHZ800);
 
+char serial_buffer[256] = {};
+unsigned int buffer_index = 0;
 unsigned int explode_color[3] = {83, 255, 255};
 
 void setup() {
@@ -47,5 +49,48 @@ void loop() {
     default:
       // UNREACHABLE
       break;
+  }
+}
+
+void serial_observer() {
+  while(Serial.available() > 0) {
+    serial_buffer[buffer_index] = Serial.read();
+    buffer_index++;
+    if (buffer_index >= 256) {
+      break;
+    }
+  }
+
+  if (buffer_index > 0) {
+    String message = String(serial_buffer);//.trim();
+    message.trim();
+    String tag = message.substring(0, 2);
+
+    if (tag == "CRG") {
+      if (currentState == WearableState::Waiting || currentState == WearableState::WaitingWithColor) {
+        Serial.println("OK. 0");
+      }
+      else {
+        Serial.println("WAM 0");
+      }
+    }
+    else if (tag == "SET") {
+      if (currentState == WearableState::Waiting) {
+        // SET ******** のスペースまでを切り取る
+        String payload = message.substring(4);
+        if (payload.length() != 8) {
+          Serial.println("INV 0");
+        }
+
+        String r = payload.substring(2, 3), g = payload.substring(4, 5), b = payload.substring(6, 7);
+        explode_color[0] = strtol(r.c_str(), NULL, 16);
+        explode_color[1] = strtol(g.c_str(), NULL, 16);
+        explode_color[2] = strtol(b.c_str(), NULL, 16);
+        currentState = WearableState::WaitingWithColor;
+      }
+      else {
+        Serial.println("WAM 0");
+      }
+    }
   }
 }
