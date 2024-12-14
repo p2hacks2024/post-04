96f0ba7c7eb68d371bb31a4f7bdc5610ce578459
takuto1127 <tiharu717@gmail.com>
2024-12-15 07:41:43 +0900

    qrのui

--- flutter/lib/view/display_qr.dart
+++ flutter/lib/view/display_qr.dart
@@ -1,6 +1,9 @@
+import 'package:design_sync/design_sync.dart';
+import 'package:epsilon_app/component/app_bar.dart';
 import 'package:epsilon_app/repository/storage_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class DisplayQr extends ConsumerWidget {
@@ -9,11 +12,57 @@ class DisplayQr extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QRコード表示'),
-      ),
-      body: QrImageView(
-        data: ref.watch(storageManagerProvider).history!.last.colorCode,
+      appBar: const MyAppBar(title: "Share"),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: EdgeInsets.all(56.adaptedWidth),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(20.adaptedWidth)),
+                  border:
+                      Border.all(color: Colors.white, width: 30.adaptedWidth)),
+              child: QrImageView(
+                data: ref.watch(storageManagerProvider).history!.last.colorCode,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 90.adaptedHeight,
+          ),
+          Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+                //border: Border.all(color: Colors.white, width: 6),
+                boxShadow: [
+                  BoxShadow(
+                    //color: Colors.white, blurStyle: BlurStyle.outer, blurRadius: 20,
+                    color: Colors.blue,
+                    blurStyle: BlurStyle.outer,
+                    blurRadius: 20,
+                  ),
+                ]),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 80.adaptedWidth, vertical: 10.adaptedHeight),
+              child: TextButton(
+                child: Text("Receive Color",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.adaptedFontSize,
+                        fontWeight: FontWeight.w600)),
+                onPressed: () {
+                  context.push('/play/play/true');
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
